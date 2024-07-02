898b8ca67af3f330429f9f9bb52fbe6d9f7ef2c5
Mouadh Mkadmi <wvagen@gmail.com>
2024-07-02 15:21:36 +0100

    Version 11.7

---
+++ Assets/_MyAssets/MyScripts/MyModels/Joint_Company_MissionLive_Model.cs
@@ -0,0 +1,9 @@
+
+namespace com.mkadmi
+{
+    public class Joint_Company_MissionLive_Model
+    {
+        public MissionLive_Model MissionLive { get; set; }
+        public Company_Model Company { get; set; }
+    }
+}
